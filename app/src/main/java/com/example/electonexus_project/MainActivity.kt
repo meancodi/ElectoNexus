e605ce4d2132a06d12b8b77fa44a8c97b9684503
Karri Sri Saya Sandeep <srisayasandeepkarri@gmail.com>
2024-08-19 16:55:58 +0530

    ..

---
+++ app/src/main/java/com/example/electonexus_project/MainActivity.kt
@@ -0,0 +1,199 @@
+package com.example.electonexus_project
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var signupButton: Button
+    private lateinit var loginButton: Button
+
+    private lateinit var un: EditText
+    private lateinit var pw: EditText
+
+    private lateinit var fbref: DatabaseReference
+
+    var type : String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        un = findViewById(R.id.LEmailAddress)
+        pw = findViewById(R.id.LPassword)
+        loginButton = findViewById(R.id.LLoginButton)
+        signupButton = findViewById(R.id.LSignupButton)
+
+
+
+        loginButton.setOnClickListener {
+            checkUsername()
+        }
+        signupButton.setOnClickListener {
+            Intent(this, Account_type::class.java).also { startActivity(it) }
+        }
+
+    }
+
+    private fun checkUsername() {
+        var b = true
+        val username = un.text.toString()
+        val password = pw.text.toString()
+        fbref =
+            FirebaseDatabase.getInstance("https://electonexusmain-default-rtdb.asia-southeast1.firebasedatabase.app")
+                .getReference("Account")
+
+        if (username.isEmpty()) {
+            un.error = "Please enter Username"
+            b = false
+        }
+        if (password.isEmpty()) {
+            pw.error = "Please enter password"
+            b = false
+        }
+        if (b) {
+            fbref.child(username).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        val tpw = dataSnapshot.child("pw").value
+                        if (tpw == password) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Login Successful",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            type = dataSnapshot.child("atype").value.toString()
+                            if(type=="A"){
+                                val eID = dataSnapshot.child("eid").value.toString()
+                                val elname = dataSnapshot.child("en").value.toString()
+                                saveCredentialsToFile(username, "A",eID,elname)
+                                Intent(this@MainActivity, admin_dashboard::class.java).also { startActivity(it) }
+                            }
+                            else if(type=="V"){
+                                Toast.makeText(this@MainActivity, "Login to Voter", Toast.LENGTH_SHORT).show()
+                                Intent(this@MainActivity, voter_sign_up::class.java).also { startActivity(it) }
+                            }
+
+                        } else {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Invalid Password",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                        }
+                    } else {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Username Not Found",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(this@MainActivity, "$error", Toast.LENGTH_SHORT).show()
+
+                }
+
+            })
+        }
+
+    }
+    private fun saveCredentialsToFile(username: String, Acctype: String,eID : String,elname : String) {
+        try {
+            // Define the file name and directory
+            val fileName = "credentials.txt"
+            val fileOutputStream: FileOutputStream = openFileOutput(fileName, Context.MODE_PRIVATE)
+            val outputStreamWriter = OutputStreamWriter(fileOutputStream)
+
+            // Write username and password to the file
+            outputStreamWriter.write("Username: $username\n")
+            outputStreamWriter.write("Acctype: $Acctype\n")
+            outputStreamWriter.write("eID: $eID\n")
+            outputStreamWriter.write("elname: $elname\n")
+
+            outputStreamWriter.close()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Toast.makeText(this, "Failed to save credentials", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkCredentialsFile() {
+        try {
+            // Define the file name
+            val fileName = "credentials.txt"
+
+            // Check if the file exists
+            val fileInputStream: FileInputStream = openFileInput(fileName)
+            val inputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder = StringBuilder()
+            var line: String?
+
+            // Read the file line by line
+            while (bufferedReader.readLine().also { line = it } != null) {
+                stringBuilder.append(line).append("\n")
+            }
+
+            // Close the reader
+            bufferedReader.close()
+
+            // Display the content (for demonstration)
+            Toast.makeText(this, "File Content:\n${stringBuilder.toString()}", Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            // If the file is not found or other errors occur
+            Toast.makeText(this, "No credentials found", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun readCredentialsFile() {
+        try {
+            // Define the file name
+            val fileName = "credentials.txt"
+
+            // Check if the file exists
+            val fileInputStream: FileInputStream = openFileInput(fileName)
+            val inputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder = StringBuilder()
+            var line: String?
+
+            // Read the file line by line
+            while (bufferedReader.readLine().also { line = it } != null) {
+                stringBuilder.append(line).append("\n")
+            }
+
+            // Close the reader
+            bufferedReader.close()
+
+            // Display the content (for demonstration)
+
+            Toast.makeText(this, "File Content:\n${stringBuilder.toString()}", Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            // If the file is not found or other errors occur
+            Toast.makeText(this, "No credentials found", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/electonexus_project/voter_sign_up.kt
@@ -0,0 +1,91 @@
+package com.example.electonexus_project
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class voter_sign_up : ComponentActivity() {
+
+    private lateinit var nameInsert: EditText
+    private lateinit var unInsert: EditText
+    private lateinit var pwInsert: EditText
+
+    private lateinit var fbref : DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_voter_sign_up)
+        nameInsert = findViewById(R.id.LVoterName)
+        unInsert = findViewById(R.id.LVoterEmailAddress)
+        pwInsert = findViewById(R.id.LVoterPassword)
+
+        fbref = FirebaseDatabase.getInstance("https://electonexusmain-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("Account")
+        val signup : Button = findViewById(R.id.LVoterSignup)
+
+        var noerror : kotlin.Boolean = true
+
+        signup.setOnClickListener {
+                noerror = saveVoterData()
+                if(noerror){
+                Intent(this, MainActivity::class.java).also { startActivity(it) }
+                Toast.makeText(this, "Signup Successful", Toast.LENGTH_SHORT).show()
+                finish()
+                }
+        }
+    }
+
+    private fun saveVoterData():Boolean{
+        var b: Boolean = true
+        val name = nameInsert.text.toString()
+        val username = unInsert.text.toString()
+        val password = pwInsert.text.toString()
+
+        if(name.isEmpty()||(name.length>10)){
+            nameInsert.error = "Please enter name under 10 characters"
+            b=false
+        }
+        if(username.isEmpty()||(username.length>10)){
+            nameInsert.error = "Please enter username under 10 characters"
+            b=false
+        }
+        if(password.isEmpty()||(password.length>10)){
+            nameInsert.error = "Please enter password under 10 characters"
+            b=false
+        }
+        if(b) {
+            fbref.child(username).addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(snapshot.exists()) {
+                        Toast.makeText(this@voter_sign_up, "Username Exists", Toast.LENGTH_SHORT)
+                            .show()
+                        b = false
+                    }
+                    else{
+                        val voter = VoterModel(name,username, password,"V")
+                        fbref.child(username).setValue(voter).addOnCompleteListener {
+                            Toast.makeText(this@voter_sign_up, "Sign Up Successful", Toast.LENGTH_SHORT).show()
+                        }.addOnFailureListener { err ->
+                            Toast.makeText(this@voter_sign_up, "${err.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(this@voter_sign_up, "$error", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+        }
+        return b
+    }
+}
